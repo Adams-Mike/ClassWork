82cdcdb9809da3612ba1a0fa855de552897d57be
Michael Adams <magnavoid@gmail.com>
2014-04-24 16:35:53 -0600

    Commit 2

--- src/gallonstoliters/GallonsToLiters.java
+++ src/gallonstoliters/GallonsToLiters.java
@@ -1,7 +1,5 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ * This is a program for converting gallons to liters.
  */
 
 package gallonstoliters;
