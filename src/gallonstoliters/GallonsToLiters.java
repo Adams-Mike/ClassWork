ac1fc37db494cf11bac88d362fbe6a42f0cdd9fe
Michael Adams <magnavoid@gmail.com>
2014-04-24 16:27:57 -0600

    Commit 1

--- src/gallonstoliters/GallonsToLiters.java
+++ src/gallonstoliters/GallonsToLiters.java
@@ -17,6 +17,12 @@ public class GallonsToLiters {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        double gallons;
+        double liters;
+        
+        gallons = 10;
+        liters = gallons * 3.7854;
+        
+        System.out.println(gallons + " gallons is" + liters + " liters");
     }
-    
 }
